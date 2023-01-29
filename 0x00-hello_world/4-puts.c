b7c58e91cbd11aafbb26dadcbaf235400d841f70
jervi00y <jervin14@student.umgc.edu>
2023-01-29 11:12:08 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bin
+#include <stdio.h>
+
+/**
+ * main - prints a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a miltilingual puzzle");
+	return (0);
+}
