a5bf2325eaecd08f14df61ea6bb7b0d1d53a0a77
jervi00y <jervin14@student.umgc.edu>
2023-01-30 04:18:59 -0800

    4. Hello, puts - Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#!/bin/bash
+
 #include <stdio.h>
-
 /**
- * main - prints a given phrase
- * Return: 0 if exited properly, non-zero otherwise
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a miltilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
