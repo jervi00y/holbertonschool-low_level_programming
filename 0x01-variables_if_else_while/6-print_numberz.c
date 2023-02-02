d6a0b79a81e31c740d07de568b8a4d5f35fe09e6
jervi00y <jervin14@student.umgc.edu>
2023-02-01 22:49:13 -0800

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
->
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes)
  */
+
 int main(void)
 {
 	int n;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(n);
-}
-
-		putchar('\n');
-
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
 	return (0);
 }
